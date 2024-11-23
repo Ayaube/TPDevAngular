5f75d3100f116e83eef2e226848cb7f0407b5e2a
aubepine <ayoub.lamchichi.info@gmail.com>
2024-11-23 12:03:30 +0100

    Projet Fini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'TP_Ayoub_LAMCHICHI';
-}
+export class AppComponent {}
